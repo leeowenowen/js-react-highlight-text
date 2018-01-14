6654a35754313b2217799a3cdcd8e0a1a3ff16d7
wangli <wangli@mobisummer.com>
2018-01-14 10:40:52 +0800

    update

--- index.js
+++ index.js
@@ -1,25 +1,36 @@
 import React from 'react';
-import './index.css'
 
 export default class HighlightText extends React.Component {
-  varructor() {
+  constructor() {
     super();
     this.instance = null;
   }
   addSpan() {
     var {
-      className,
+      normalTextClsName,
       highlightTextClsName,
       text,
       keyword,
+      ignoreCase,
+      highlightAllMatch,
     } = this.props;
     var container = document.createElement('span');
     container.className = 'HighlightText_normal';
+    if (normalTextClsName) {
+      container.className = normalTextClsName;
+    }
     var highlightTextCls = 'HighlightText_highlight';
     if (highlightTextClsName) {
-      highlightTextCls = [highlightTextCls, highlightTextClsName].join(' ');
+      highlightTextCls = highlightTextClsName;
     }
-    var spanHtml = text.replace(new RegExp(keyword, 'gi'), '<span class=' + highlightTextCls + '>$&</span>');
+    var params = '';
+    if (ignoreCase) {
+      params = params + 'i';
+    }
+    if (highlightAllMatch) {
+      params = params + 'g';
+    }
+    var spanHtml = text.replace(new RegExp(keyword, params), '<span class=' + highlightTextCls + '>$&</span>');
     container.innerHTML = spanHtml;
     this.instance.appendChild(container);
   }
@@ -43,7 +54,7 @@ export default class HighlightText extends React.Component {
     this.instance = ele;
   }
   render() {
-    return <div ref = { this.updateRef.bind(this) }
+    return <span ref = { this.updateRef.bind(this) }
     />;
   }
 }
